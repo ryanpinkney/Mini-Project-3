b18aeedb831e6769ab6d7d38f0a4f1b367261064
Jeff <jeff.aj.thomson@gmail.com>
2020-04-02 19:16:15 -0700

    part 1 now uses bit operations

--- MemMan.c
+++ MemMan.c
@@ -5,55 +5,33 @@
 
 int main() {
 
-int n = 0;
-int m = 0;
-char fname[100];
-//section to input the name of the file that will be read
-printf("what file name are you using: ");
-scanf("%s", fname);
-FILE *fptr = fopen(fname, "r");
-
-fscanf(fptr, "%d", &n);
-fscanf(fptr, "%d", &m);
-//outputs the first two values to make it easy to check for accuracy
-printf("%d \n", n);
-printf("%d \n", m);
-
-long address;
-//the while loop designed to find and output page numbers and offsets
-while(fscanf(fptr, "%ld", &address) != EOF){
+	int n;
+	int m;
+	char fname[100];
 	
-	//important data is stored here
-	long utility = address;
-	long page = 0;
-	long offset = 0;
+	//section to input the name of the file that will be read
+	printf("what file name are you using: ");
+	scanf("%s", fname);
+	FILE *fptr = fopen(fname, "r");
+
+	fscanf(fptr, "%d", &n);
+	fscanf(fptr, "%d", &m);
+
+	//outputs the first two values to make it easy to check for accuracy
+	printf("n=%d \n", n);
+	printf("m=%d \n", m);
+
+	int address;
 	
-	printf("utility is now %ld \n", utility);
+	//the while loop designed to find and output page numbers and offsets
+	while(fscanf(fptr, "%d", &address) != EOF){
 
-	//the offset is calculated and stored
-	for(int n2 = 0; n2 < n; n2++){
+		//important data is stored here
+		int page = address >> n;
+		int offset = address | (0xffff >> m);
 
-		//the offset is incremented by the remainder
-		int rem = utility % 10;
-		rem = rem<<n2;
-		offset = offset + rem;
-		printf("offset is now %ld \n", offset);
-		utility = utility/10;
-		printf("utility is now %ld \n", utility);
-	}
-	//the page is calculated and stored
-	for(int m2 = 0; m2 < m; m2++){
-
-		//the page is incremented by the remainder
-		int rem = utility % 10;
-		rem = rem<<m2;
-		page = page + rem;
-		printf("page is now %ld \n", page);
-		utility = utility/10;
-		printf("utility is now %ld \n", utility);
+		printf("Virtual address %i is in page number %i and offset %i \n", address, page, offset);
+		
 	}
 
-
-	printf("Virtual address %ld is in page number %ld and offset %ld \n", address, page, offset);
-}
 }
