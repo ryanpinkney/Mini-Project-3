0bc357ac6d506623467d09997f280adc12d2f7af
Jeff <jeff.aj.thomson@gmail.com>
2020-04-04 20:42:25 -0700

    fs_create and fs_ls appear working

--- FileSys.c
+++ FileSys.c
@@ -1,11 +1,12 @@
+#include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include <errno.h>
 
 typedef struct {
-	char name[8];
+	char name[9];
 	int size;
 	int blockPointers[8];
 	int used;
@@ -16,11 +17,125 @@ typedef struct {
 	INode nodes[16];
 } SuperBlock;
 
-char *name;
+char *diskname;
 int fdesc;
+SuperBlock sb;
+
+int writesb() {
+
+	// Buffer for writing
+	char buffer[1024];
+	
+	// Scanning position in buffer
+	int pos = 0;
+
+	// Insert free block list
+	for(int i = 0; i < 128; i++) {
+		buffer[pos++] = sb.fbl[i];
+	}
+
+	// Insert index nodes
+	for(int i = 0; i < 16; i++) {
+
+		// Insert name
+		for(int j = 0; j < 8; j++) {
+			buffer[pos++] = sb.nodes[i].name[j];
+		}
+
+		// Insert size
+		for(int j = 0; j < 4; j++) {
+			buffer[pos++] = (sb.nodes[i].size >> ((3 - j) << 3)) & 0xff;
+		}
+
+		// Insert block pointers
+		for(int j = 0; j < 8; j++) {
+			for(int k = 0; k < 4; k++) {
+				buffer[pos++] = (sb.nodes[i].blockPointers[j] >> ((3 - k) << 3)) & 0xff;
+			}
+		}
+
+		// Extract used boolean
+		for(int j = 0; j < 4; j++) {
+			buffer[pos++] = (sb.nodes[i].used >> ((3 - j) << 3)) & 0xff;
+		}
+
+	}
+
+	// Write buffer to disk
+	if(pwrite(fdesc, buffer, 1024, 0) == -1) {
+		fprintf(stderr, "Writing %s failed: %i\n", diskname, errno);
+		return -1;
+	}
+
+	return 0;
+
+}
 
 int fs_create(char name[], int size) {
+
+	// Check input parameters
+	if(strlen(name) > 8) {
+		fprintf(stderr, "Cannot create file %s: file name too long.\n", name);
+		return -1;
+	}
+	if(size > 8) {
+		fprintf(stderr, "Cannot create file %s: requested size too large \n", name);
+		return -1;
+	}
+	
+	// Search for a free node
+	// Also check if file with given name already exists
+	int freenode = -1;
+	for(int i = 0; i < 16; i++) {
+		if(sb.nodes[i].used == 0) {
+			freenode = i;
+		} else if(strcmp(sb.nodes[i].name, name) == 0) {
+			fprintf(stderr, "Cannot create file %s: file named %s already exists.\n", name, sb.nodes[i].name);
+			return -1;
+		}
+	}
+
+	// Print failure message if there is no free node
+	if(freenode == -1) {
+		fprintf(stderr, "Cannot create file %s: maximum number of files reached.\n", name);
+		return -1;
+	}
+
+	// Search for free blocks
+	int freeblocks[size];
+	int blockcount;
+	for(int i = 0; i < 128 && blockcount < size; i++) {
+		if(sb.fbl[i] == 0)
+			freeblocks[blockcount++] = i;
+	}
+
+	// Print failure message if there are not enough free blocks
+	if(blockcount < size) {
+		fprintf(stderr, "Cannot create file %s: not enough free data blocks.\n", name);
+		return -1;
+	}
+
+	// Allocate free node
+	for(int i = 0; i < strlen(name); i++) {
+		sb.nodes[freenode].name[i] = name[i];
+	}
+	sb.nodes[freenode].name[strlen(name)] = 0;
+	sb.nodes[freenode].size = size;
+	sb.nodes[freenode].used = 1;
+
+	// Allocate free blocks
+	for(int i = 0; i < size; i++) {
+		sb.fbl[freeblocks[i]] = 1;
+		sb.nodes[freenode].blockPointers[i] = freeblocks[i];
+	}
+
+	if(writesb() == -1)
+		return -1;
+	else
+		printf("Write complete for file %s of size %i\n", name, size);
+
 	return 0;
+
 }
 
 int fs_delete(char name[]) {
@@ -28,7 +143,12 @@ int fs_delete(char name[]) {
 }
 
 int fs_ls() {
-	return 0;
+	
+	for(int i = 0; i < 16; i++) {
+		if(sb.nodes[i].used)
+			printf("%s %i\n", sb.nodes[i].name, sb.nodes[i].size);
+	}
+
 }
 
 int fs_read(char name[], int blockNum, char buf[]) {
@@ -48,25 +168,24 @@ int main(int argc, char *argv[]) {
 	}
 
 	// Take filesystem name from args
-	name = argv[1];
+	diskname = argv[1];
 	
 	// Open filesystem and store descriptor
 	fdesc = open(argv[1], O_RDWR);
 	if(fdesc == -1) {
-		fprintf(stderr, "Opening %s failed: %i\n", name, errno);
+		fprintf(stderr, "Opening %s failed: %i\n", diskname, errno);
 		exit(1);
 	}
 
 	// Parse super block from first 1KB
-	printf("Reading super block of %s...", name);
-	SuperBlock sb;
+	printf("Reading super block of %s...", diskname);
 
 	// Buffer for reading and writing
 	char buffer[1024];
 
 	// Read full super block into buffer
 	if(pread(fdesc, buffer, 1024, 0) == -1) {
-		fprintf(stderr, "Reading %s failed: %i\n", name, errno);
+		fprintf(stderr, "Reading %s failed: %i\n", diskname, errno);
 		exit(1);
 	}
 	
@@ -78,7 +197,7 @@ int main(int argc, char *argv[]) {
 		sb.fbl[i] = buffer[pos++];
 	}
 
-	// Extract INodes
+	// Extract index nodes
 	for(int i = 0; i < 16; i++) {
 
 		// Extract name
@@ -111,6 +230,7 @@ int main(int argc, char *argv[]) {
 	printf(" Complete\n");
 
 	// Actions on the filesystem can now be taken here
+	fs_ls();
 
 	// Close filesystem
 	close(fdesc);
